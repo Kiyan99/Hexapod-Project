c3df3065c2da0ce593b90206ebb39280b1e85c63
Kiyan <kiyan.ahmadi@students.plymouth.ac.uk>
2025-07-30 12:24:17 +0100

    30/07/2025
    I have changed the code to simulate the walking gait of an ant or tripod-gait.
    front, read and middle legs function differently based on their position.
    commented each part of the code.

--- Hexapod_Project/Hexapod_test/Hexapod_test.ino
+++ Hexapod_Project/Hexapod_test/Hexapod_test.ino
@@ -24,11 +24,11 @@ struct Leg {
 
   Adafruit_PWMServoDriver* pwm; // Pointer to the correct board
 
-  int x, y, z;
-  double c, L;
+  int x, y, z;  // x, y, z axis values
+  double c, L;  // c, L values as double to store more accurate number
 
-  int theta1_deg, theta2_deg, theta3_deg;
-  int theta1_pwm, theta2_pwm, theta3_pwm;
+  int theta1_deg, theta2_deg, theta3_deg;  // theta angles in degrees
+  int theta1_pwm, theta2_pwm, theta3_pwm;  // theta angles in PWM
 };
 
 // Define four legs and assign their PWM channels
@@ -39,6 +39,7 @@ Leg leg4 = {8, 9, 10, &pwm2}; // front right
 Leg leg5 = {0, 1, 2, &pwm2}; // left middle
 Leg leg6 = {8, 9, 10, &pwm1}; // rear right
 
+// Storing all legs in one array
 Leg* allLegs[6] = { &leg1, &leg2, &leg3, &leg4, &leg5, &leg6 };
 
 // Inverse Kinematics for one leg
@@ -49,11 +50,13 @@ void computeIK(Leg &leg, int x, int y, int z) {
 
   leg.L = sqrt(x * x + z * z);
   leg.c = sqrt(leg.L * leg.L + y * y);
-
+  
+  // Calculating theta angles in radians
   double theta1_rad = atan2(x, z);
   double theta2_rad = acos((pow(link_1, 2) + pow(leg.c, 2) - pow(link_2, 2)) / (2 * link_1 * leg.c)) + atan2(y, leg.L);
   double theta3_rad = acos((pow(link_1, 2) + pow(link_2, 2) - pow(leg.c, 2)) / (2 * link_1 * link_2));
 
+  // Converting theta angles to degrees
   leg.theta1_deg = theta1_rad * (180.0 / M_PI);
   leg.theta2_deg = theta2_rad * (180.0 / M_PI);
   leg.theta3_deg = 180 - (theta3_rad * (180.0 / M_PI));
@@ -61,10 +64,12 @@ void computeIK(Leg &leg, int x, int y, int z) {
 
 // Move servos for one leg
 void moveLeg(Leg &leg) {
+  // Maping theta angles from degrees to PWM
   leg.theta1_pwm = map(leg.theta1_deg, 0, 180, SERVO_MIN, SERVO_MAX);
   leg.theta2_pwm = map(leg.theta2_deg, 0, 180, SERVO_MIN, SERVO_MAX);
   leg.theta3_pwm = map(leg.theta3_deg, 0, 180, SERVO_MIN, SERVO_MAX);
-  // first board
+  
+  // Sending PWM signals to each baord and legs 
   leg.pwm->setPWM(leg.servo0, 0, leg.theta1_pwm);
   delay(5);
   leg.pwm->setPWM(leg.servo1, 0, leg.theta2_pwm);
@@ -75,6 +80,7 @@ void moveLeg(Leg &leg) {
 }
 
 void setup() {
+  // initialise boards and serial monitor
   Serial.begin(9600);
   pwm1.begin();
   pwm1.setPWMFreq(50);
@@ -90,82 +96,86 @@ void setup() {
     moveLeg(*allLegs[i]);
   }
 
-  Serial.print("Motor 1: ");
-  Serial.print(leg1.theta1_pwm);
-  Serial.print(" Motor 2: ");
-  Serial.print(leg1.theta2_pwm);
-  Serial.print(" Motor 3: ");
-  Serial.println(leg1.theta3_pwm);
-
-
-  delay(2000);
+  delay(4000);
 }
 
 void loop() {
 
+  // for-loop will run 10 timmes
+  for (float t = 0; t <= 1.0; t += 0.1) {
     
-  for (float t = 0; t <= 1.0; t += 0.05) {
+    // Moving forward
     int z = (1 - t) * -30 + t * 30;              // Forward swing
     int y = global_y + 30 * sin(M_PI * t);           // Lift arc
-    computeIK(leg1, global_X, y, z);
+
+
+    // Front legs
+    // Pulling and pushing values used by front and read legs
+    int z_push = (1 - t) * 0 + t * 60;
+    int z_pull = (1 - t) * 60;  
+    int x_push = (1 - t) * 50 + t * 100;
+    int x_pull = (1 - t) * 100 + t * 50;  
+    computeIK(leg1, x_push, y, z_push);
+    computeIK(leg4, x_pull, global_y, -z_pull);
+    moveLeg(leg1);
+    moveLeg(leg4);
+
+    // Middle legs
+    // Pulling back for middle legs
+    int z_mid_pull = (1 - t) * 30 + t * -30;
     computeIK(leg2, global_X, y, -z);
-    computeIK(leg3, global_X, y, z);
-    moveLeg(leg1);
-    moveLeg(leg2);
+    computeIK(leg5, global_X, global_y, z_mid_pull);  
+    moveLeg(leg2); 
+    moveLeg(leg5);     
+
+
+    // Rear legs    
+    computeIK(leg3, x_pull, y, -z_pull);
+    computeIK(leg6, x_push, global_y, z_push);
     moveLeg(leg3);
-    
-    int z_pull = (1 - t) * 30 + t * -30;  // Pull back smoothly
-    computeIK(leg4, global_X, global_y, -z_pull);
-    computeIK(leg5, global_X, global_y, z_pull);
-    computeIK(leg6, global_X, global_y, -z_pull);
-    moveLeg(leg4);
-    moveLeg(leg5);
-    moveLeg(leg6);
+    moveLeg(leg6);    
+
+
   }
 
 
 
-  for (float t = 0; t <= 1.0; t += 0.05) {
+  for (float t = 0; t <= 1.0; t += 0.1) {
+    
+    // Moving forward
     int z = (1 - t) * -30 + t * 30;              // Forward swing
     int y = global_y + 30 * sin(M_PI * t);           // Lift arc
-    computeIK(leg4, global_X, y, -z);
-    computeIK(leg5, global_X, y, z);
-    computeIK(leg6, global_X, y, -z);
-    moveLeg(leg4);
-    moveLeg(leg5);
-    moveLeg(leg6);
 
-    int z_pull = (1 - t) * 30 + t * -30;  // Pull back smoothly
-    computeIK(leg1, global_X, global_y, z_pull);
-    computeIK(leg2, global_X, global_y, -z_pull);
-    computeIK(leg3, global_X, global_y, z_pull);
+
+    //front Legs
+    int z_push = (1 - t) * 0 + t * 60;
+    int z_pull = (1 - t) * 60;  
+    int x_push = (1 - t) * 50 + t * 100;
+    int x_pull = (1 - t) * 100 + t * 50;  
+    computeIK(leg4, x_push, y, -z_push);
+    computeIK(leg1, x_pull, global_y, z_pull);
+    moveLeg(leg4);
     moveLeg(leg1);
+
+
+
+    // Midlle Legs
+    // Pulling back
+    int z_mid_pull = (1 - t) * 30 + t * -30;
+    computeIK(leg5, global_X, y, z);
+    computeIK(leg2, global_X, global_y, -z_mid_pull);
+    moveLeg(leg5);
     moveLeg(leg2);
+
+
+
+    // Rear legs
+    computeIK(leg6, x_pull, y, z_pull);
+    computeIK(leg3, x_push, global_y, -z_push);
+    moveLeg(leg6);
     moveLeg(leg3);
 
   }
-
-
-
-
-  // for (float t = 0; t <= 1.0; t += 0.05) {
-  //   int z = (1 - t) * 30 + t * -30;  // Pull back smoothly
-
-  //   // All legs stay on ground (no lifting Y)
-  //   computeIK(leg1, global_X, global_y, z);
-  //   computeIK(leg2, global_X, global_y, -z);
-  //   computeIK(leg3, global_X, global_y, z);
-  //   computeIK(leg4, global_X, global_y, -z);
-  //   computeIK(leg5, global_X, global_y, z);
-  //   computeIK(leg6, global_X, global_y, -z);
-
-  //   moveLeg(leg1);
-  //   moveLeg(leg2);
-  //   moveLeg(leg3);
-  //   moveLeg(leg4);
-  //   moveLeg(leg5);
-  //   moveLeg(leg6);
-  // }
 
 
 
